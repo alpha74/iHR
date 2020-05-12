23d79ca9f1ff7cb3a4285141234c1d4c28207b13
Aman Kumar <amanalphakumar@gmail.com>
2020-05-12 16:05:06 +0530

    Create Input_and_Output.cpp

---
+++ C++/Input_and_Output.cpp
@@ -0,0 +1,19 @@
+// https://www.hackerrank.com/challenges/cpp-input-and-output/problem
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() 
+{
+    int num1 = 0, num2 = 0, num3 = 0 ;
+    cin >> num1 >> num2 >> num3 ;
+
+    cout << num1+num2+num3 ;
+
+    return 0;
+}
