e2f275baac69fb7d4d4adf05e83d95e89488a1b0
Aman Kumar <amanalphakumar@gmail.com>
2020-05-14 00:55:28 +0530

    Create Pointer.cpp

---
+++ C++/Pointer.cpp
@@ -0,0 +1,23 @@
+// https://www.hackerrank.com/challenges/c-tutorial-pointer/problem
+
+#include <stdio.h>
+#include <math.h>
+
+void update(int *a,int *b) 
+{
+    int temp1 = *a, temp2 = *b ;
+
+    *a = temp1 + temp2 ;
+    *b = abs( temp1 - temp2 ) ;      
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
