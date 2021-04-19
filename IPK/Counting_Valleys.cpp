8acee356111bf1b6f5c8ea7443b344625bf7efe0
Aman Kumar <amanalphakumar@gmail.com>
2021-04-19 23:26:24 +0530

    Create Counting_Valleys.cpp

---
+++ IPK/Counting_Valleys.cpp
@@ -0,0 +1,81 @@
+// https://www.hackerrank.com/challenges/counting-valleys/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup&h_r=next-challenge&h_v=zen
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'countingValleys' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER steps
+ *  2. STRING path
+ */
+
+int countingValleys(int steps, string path) {
+    int level = 0 ;
+    int valley = 0 ;
+    
+    if(steps > 0 )
+    {       
+        for( int i = 0 ; i < steps ; i++ )
+        {
+            if( path[i] == 'U')
+                level++ ;
+            else
+                level-- ;
+            
+            if( level == 0 && path[i] == 'U' )
+                valley++ ;
+        }
+    }
+    
+    return valley ;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string steps_temp;
+    getline(cin, steps_temp);
+
+    int steps = stoi(ltrim(rtrim(steps_temp)));
+
+    string path;
+    getline(cin, path);
+
+    int result = countingValleys(steps, path);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
